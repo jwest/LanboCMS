f9de93e2357a0b0906140c9e66841c50d93c9729
Jakub Westfalewski <jwest@jwest.pl>
2012-06-09 14:18:26 +0200

    config file

---
+++ config/lanbocms.php
@@ -0,0 +1,12 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+return array
+(
+	'objects' => array
+    (
+        'pages' => NULL,
+        'articles' => NULL,
+    ),
+
+    'wysiwyg' => 'redactor',
+);
